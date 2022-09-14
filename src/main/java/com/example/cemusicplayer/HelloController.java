325b058cdf8b053427c8cf02b1757bc6c1c21378
Kun Kin Zheng Liang <106352914+kunZhen@users.noreply.github.com>
2022-09-14 17:46:03 -0600

    Se comienza con la creación de la vistaLogin

--- src/main/java/com/example/cemusicplayer/HelloController.java
+++ src/main/java/com/example/cemusicplayer/HelloController.java
@@ -1,14 +1,6 @@
 package com.example.cemusicplayer;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
