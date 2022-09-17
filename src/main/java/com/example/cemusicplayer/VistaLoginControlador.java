d6f604bb9bf7480a19e8f82c789d21df8b64f016
MeGM4 <104104964+MeGM4@users.noreply.github.com>
2022-09-16 22:55:17 -0600

    Se cambia la lectura de archivos json por csv, se añaden labels para errores

--- src/main/java/com/example/cemusicplayer/VistaLoginControlador.java
+++ src/main/java/com/example/cemusicplayer/VistaLoginControlador.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -19,14 +20,14 @@ import javafx.stage.Window;
 import javafx.stage.WindowEvent;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.google.gson.Gson;
 public class VistaLoginControlador implements Initializable {
+    @FXML
+    public Label labelError;
     @FXML
     private TextField TextoUsuario; //Objetos con el mismo ID que en scene builder
 
@@ -43,10 +44,12 @@ public class VistaLoginControlador implements Initializable {
         if (evt.equals(TextoUsuario)) {
             if (event.getCharacter().equals(" ")) { // No permite colocar espacios
                 TextoUsuario.clear();
+                labelError.setText("Error: No puede ingresar espacios.");
             }
         } else if (evt.equals(TextoContrasena)) {
             if (event.getCharacter().equals(" ")) {
                 TextoContrasena.clear();
+                labelError.setText("Error: No puede ingresar espacios.");
             }
         }
 
@@ -56,7 +59,8 @@ public class VistaLoginControlador implements Initializable {
     private void eventAction(ActionEvent event){
         Object evt = event.getSource();
 
-        cargarArchivo();
+        //cargarArchivo();
+        leerLineaCSV();
 
         if (evt.equals(BotonIngresar)){
             System.out.println("Boton ingresar");
@@ -68,37 +72,62 @@ public class VistaLoginControlador implements Initializable {
                 CargarEscena("vistaPrincipal.fxml", event); //Crear nueva ventana
 
             }else {
-                System.out.println("Error");
+                labelError.setText("Error: No puede dejar el espacio en blanco.");
             }
         }
     }
 
+    /*
+    public String cargarArchivo(){
 
-    public void cargarArchivo(){
-        Gson gson = new Gson();
         String fichero = "";
+        System.out.println("entra a cargar archivo");
+        String ruta = "src/main/resources/com/example/cemusicplayer/infoUsuariosPrueba.csv";
 
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Usuario\\Documents\\II Semestre\\PruebaCEMusicPlayer\\CEMusicPlayer\\src\\main\\resources\\com\\example\\cemusicplayer\\infoUsuariosPrueba.csv"))) {
+        try  {
+            System.out.println("entra al try");
+            BufferedReader br = new BufferedReader(new FileReader(ruta));
             String linea;
-            br.readLine();
-            while ((fichero = br.readLine()) != null) {
 
-                String[] row = fichero.split(";");
+            while ((linea = br.readLine()) != null) {
+                fichero += (linea+"/");
+
+                 String[] row = fichero.split(";");
+
+                Usuario usuario = new Usuario(row[0], row[1], row[2], row[3]);
+                System.out.println("usuario");
+
+
+            }
+            //br.close();
+
+        } catch (IOException e) {
+            System.err.println("Error al abrir el archivo");
+        }
+
+
+        return fichero;
+    } */
+
+    public String leerLineaCSV(){
+        String linea = "";
+        String ruta = "src/main/resources/com/example/cemusicplayer/infoUsuariosPrueba.csv";
+        System.out.println("entra a leer linea");
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(ruta));
+            String bfRead;
+            if ((bfRead = bf.readLine()) != null){
+                linea = bfRead;
+                String[] row = linea.split(";");
 
                 Usuario usuario = new Usuario(row[0], row[1], row[2], row[3]);
                 System.out.println(usuario);
-
-
             }
-            br.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e){
+                System.err.println("Error leyeno archivo csv");
+            } return linea;
         }
 
-
-    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
