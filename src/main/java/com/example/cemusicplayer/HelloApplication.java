62c69637191332ff6f76fd6eaa86048117abd95e
Kun Kin Zheng Liang <106352914+kunZhen@users.noreply.github.com>
2022-09-17 20:32:38 -0600

    Se evita el resize de la ventana y se agrega una banda de color al login

--- src/main/java/com/example/cemusicplayer/HelloApplication.java
+++ src/main/java/com/example/cemusicplayer/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vistaLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("CE Music Player!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
         /*
