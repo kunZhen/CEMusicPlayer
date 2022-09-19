112db4414489cb5af9c549bd73f490c81abe34db
MeGM4 <monicaegm4@gmail.com>
2022-09-18 22:11:33 -0600

    se asigna ID a los componentes de las ventanas, se colocan las variables en los controladores de biblioteca y playlist.

--- src/main/java/com/example/cemusicplayer/VistaBibliotecaControlador.java
+++ src/main/java/com/example/cemusicplayer/VistaBibliotecaControlador.java
@@ -1,12 +1,47 @@
 package com.example.cemusicplayer;
 
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VistaBibliotecaControlador implements Initializable {
+
+    @FXML
+    private TextField addBibliotecaTextField;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TableView<?> bibliotecaTableView;
+
+    @FXML
+    private TableColumn<?, ?> cantidadColumn;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TableColumn<?, ?> fechaColumn;
+
+    @FXML
+    private TableColumn<?, ?> nombreColumn;
+
+    @FXML
+    private Button openButton;
+
+    @FXML
+    private Text searchText;
+
+    @FXML
+    private TextField searchTextField;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/main/java/com/example/cemusicplayer/VistaPlaylistControlador.java
+++ src/main/java/com/example/cemusicplayer/VistaPlaylistControlador.java
@@ -1,11 +1,82 @@
 package com.example.cemusicplayer;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VistaPlaylistControlador implements Initializable {
+
+    @FXML
+    private Label bibliotecaLabel;
+
+    @FXML
+    private Button cycleButton;
+
+    @FXML
+    private ImageView cycleImage;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button favoriteButton;
+
+    @FXML
+    private ImageView favoriteImage;
+
+    @FXML
+    private Button garbageButton;
+
+    @FXML
+    private ImageView garbageImage;
+
+    @FXML
+    private Label labelArtist;
+
+    @FXML
+    private Label labelSong;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private ImageView nextImage;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private ImageView playImage;
+
+    @FXML
+    private Button playSelectedButton;
+
+    @FXML
+    private ImageView previousButton;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private ListView<?> songsListView;
+
+    @FXML
+    private Button uploadButton;
+
+    @FXML
+    private ImageView uploadImage;
+
+    @FXML
+    private Slider volumSlider;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
